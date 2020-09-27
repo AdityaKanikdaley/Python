b379c01ea95804ed839486736d28b2c8fb83419a
Aditya Kanikdaley <adikanikdaley@gmail.com>
2020-09-27 22:20:19 +0530

    commit

---
+++ Classes.py
@@ -0,0 +1,10 @@
+class map():
+   def __init__(self,material):
+        self.material = 'Old house'
+        print(f'I build {material}')
+
+
+house1 = map("New Material 1")
+house2 = map("New Material 2")
+house3 = map("New Material 3")
+house4 = map("New Material 4")
