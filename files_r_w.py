ff6577df0874ebc269d09df1e8e88e149d6eb15e
AdityaKanikdaley <66816491+AdityaKanikdaley@users.noreply.github.com>
2020-10-14 13:13:15 +0530

    commit

--- files_r_w.py
+++ files_r_w.py
@@ -1,29 +1,46 @@
+# #%%
+
+############## Udemy #####################
+
+# # file= open("text.txt","r")
+# # for line in file:
+# #    if 'python' in line.lower():
+# #        print(line)
+# # file.close()
+
+# # print('x'*20)
+# # print('\n')
+
+# # with open('text.txt','r') as file:
+# #    for line in file:
+# #       if 'python' in line.lower():    
+# #         print(line)
+
+# # print('x'*20)
+# # print('\n')
+
+# # with open('text.txt','r') as file:
+# #       line = file.readline()
+# #       while line:
+# #          print(line,end="")
+# #          line = file.readline()
+
+# with open('text.txt','r') as file:
+#    lines_list = file.readlines()
+# for line in lines_list[::-1]:
+#    print(line,end="")
+
+# #%%
+
+
+############ Class #############
 #%%
-# file= open("text.txt","r")
-# for line in file:
-#    if 'python' in line.lower():
-#        print(line)
-# file.close()
+file1 = open("python.txt","w")
+file1.write("fdfdfddfdf")
+file1.write("111111111111111111111fdfdfddfdf")
 
-# print('x'*20)
-# print('\n')
+print("file created")
+file1.close()
 
-# with open('text.txt','r') as file:
-#    for line in file:
-#       if 'python' in line.lower():    
-#         print(line)
 
-# print('x'*20)
-# print('\n')
-
-# with open('text.txt','r') as file:
-#       line = file.readline()
-#       while line:
-#          print(line,end="")
-#          line = file.readline()
-
-with open('text.txt','r') as file:
-   lines_list = file.readlines()
-for line in lines_list[::-1]:
-   print(line,end="")
-
+# %%
