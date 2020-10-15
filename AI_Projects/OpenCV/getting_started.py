d268240eb7d1c5f8e7e1f5f86fd7f528ff47b147
AdityaKanikdaley <66816491+AdityaKanikdaley@users.noreply.github.com>
2020-10-15 17:34:34 +0530

    commit

--- AI_Projects/OpenCV/getting_started.py
+++ AI_Projects/OpenCV/getting_started.py
@@ -1,17 +1,19 @@
 import cv2  
-import os
+import numpy as np
 
 # read an image 
 # int 1  = loads colored image 
 # int 0  = loads grey scale image 
 # int -1 = loads image as such including alpha channel
-v = 'D:\Python\AI_Projects\OpenCV\lena.jpg'
-img = cv2.imread(v)
-cv2.imshow('image', img)
-k = cv2.waitKey(0)
 
-if k == 27:
-    cv2.destroyAllWindows()
-elif k == ord('s'):
-    cv2.imwrite('lena_copy.png',img)
+v = 'lena.jpg'
+img = cv2.imread(v)
+print(img)
+
+# k = cv2.waitKey(0)
+
+# if k == 27:
+#     cv2.destroyAllWindows()
+# elif k == ord('s'):
+#     cv2.imwrite('lena_copy.png',img)
 
