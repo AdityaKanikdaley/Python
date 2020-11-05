df537d65c64906a51b68e91ad6131f3903308101
AdityaKanikdaley <66816491+AdityaKanikdaley@users.noreply.github.com>
2020-11-05 21:52:45 +0530

    commit

--- duplicate_removal(list).py
+++ duplicate_removal(list).py
@@ -5,3 +5,10 @@ for i in range(10):
 list1 = list(dict.fromkeys(list1))
  # or use list1 = list(set(list1))
 print(list1)
+
+######### f string ########
+
+# a=2
+# b=2
+# m = f'Aditya is {a} and {b}'
+# print(m)
